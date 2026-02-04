4870dfee8942d93d3e8fbbb98b0d779162dd6abc
Xxshadowreaper1 <84533497+Xxshadowreaper1@users.noreply.github.com>
2026-02-04 21:05:27 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+const tagUlinkPlayers = [
+    { id: "keiran-edwards", name: "Keiran Edwards", count: 1, lastTagged: "3/2/26" },
+    { id: "jacob-benbow", name: "Jacob Benbow", count: 0, lastTagged: "" },
+    { id: "fin-francis", name: "Fin Francis", count: 0, lastTagged: "" },
+    { id: "george-mcvicar", name: "George Mcvicar", count: 2, lastTagged: "4/2/26" },
+    { id: "griff-gittins", name: "Griff Gittins", count: 0, lastTagged: "" },
+    { id: "kieron-neary", name: "Kieron Neary", count: 1, lastTagged: "3/2/26" },
+    { id: "jake-willcock", name: "Jake Willcock", count: 0, lastTagged: "" },
+    { id: "aarush-thakur", name: "Aarush Thakur", count: 1, lastTagged: "4/2/26" },
+    { id: "woody-jones", name: "Woody Jones", count: 0, lastTagged: "" }
+];
+
+window.tagUlinkPlayers = tagUlinkPlayers;
